7ae085a00088793f4192cf7bc54d63578f7cf04a
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-08-22 01:18:42 -0400

    Adds auth route to check authorization status of current user

---
+++ functions/utilities/auth.js
@@ -0,0 +1,36 @@
+const { admin } = require('./admin');
+const { db } = require('./admin');
+
+
+exports.auth = (req, res, next) => {
+    let idToken;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('email', '==', req.user.email)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            name = data.docs[0].data().name;
+            return next();
+        })
+        .catch(err => {
+            if (err.code === 'auth/argument-error') {
+                return res.status(401).json({ token: 'This is an unauthorized token, please use a valid token' })
+            } else {
+                console.error('Error while verifying token ', err);
+                return res.status(403).json(err);
+            }
+        })
+
+}
