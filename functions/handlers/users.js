1f84926347221697b1e2b994f164bb42d8386727
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-08-21 22:47:16 -0400

    Adds handler functions file

---
+++ functions/handlers/users.js
@@ -0,0 +1,5 @@
+const { db, admin, firebase } = require('../utilities/admin');
+
+const { config } = require('../utilities/config')
+
+const { validateSignupData, validateLoginData } = require('../utilities/validators');
