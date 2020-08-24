6af62eec49ae7ee0db50e9126547e3c6013c6b9e
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-08-24 13:10:22 -0400

    Adds generate text function based on user preference

--- functions/handlers/data.js
+++ functions/handlers/data.js
@@ -18,23 +18,53 @@ exports.getTitles = (req, res) => {
 
 
 exports.generateText = (req, res) => {
+    console.log(name);
+    console.log(email);
 
     // const sentence = txtgen.sentence();
     // const paragraph = txtgen.paragraph();
     // const article = txtgen.article();
 
-    let result;
-    const type = req.params.type
+    let type;
+    db.collection('users').where("email", "==", email).get()
+        .then((data) => {
+            let userData = [];
+            data.forEach((doc) => {
+                userData.push(doc.data());
+            })
+            if (!Array.isArray(userData) || !userData.length) {
+                console.log("no such user")
+                result = txtgen.sentence();
+                return res.json({result});
+            } else {
+                console.log(userData[0].email)
+                type = userData[0].preference;
+                console.log(type);
 
-    if (type === "sentence") {
-        result = txtgen.sentence();
-    } else if (type === "paragraph") {
-        result = txtgen.paragraph();
-    } else if (type === "article") {
-        result = txtgen.article();
-    }
-    
-    return res.json({result});
+                if (type == null || type == undefined || typeof type == undefined) {
+                    console.log("User has no preference")
+                    result = txtgen.sentence();
+                    return res.json({result});
+                } else {
+                    if (type === "sentence") {
+                        result = txtgen.sentence();
+                    } else if (type === "paragraph") {
+                        result = txtgen.paragraph();
+                    } else {
+                        result = txtgen.sentence();
+                    }
+                    console.log(type);
+                    console.log(result)
+                    return res.json({result});
+                }
+            }
+        })
+        .catch((error) => {
+        console.error(error);
+        console.error(error);
+        return res.status(500).json({error: "Something went wrong"});
+    })
+        
 
 }
 
