b3d9cade6daebcddbf13304008e75c345e271287
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-08-24 13:08:18 -0400

    Adds basic auth to generate sentences for users not logged in

--- functions/utilities/auth.js
+++ functions/utilities/auth.js
@@ -4,7 +4,6 @@ const { db } = require('./admin');
 
 exports.auth = (req, res, next) => {
     let idToken;
-
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
         idToken = req.headers.authorization.split('Bearer ')[1];
     } else {
@@ -15,13 +14,13 @@ exports.auth = (req, res, next) => {
     admin.auth().verifyIdToken(idToken)
         .then(decodedToken => {
             req.user = decodedToken;
-            email = req.user.email;
             return db.collection('users')
                 .where('email', '==', req.user.email)
                 .limit(1)
                 .get();
         })
         .then(data => {
+            email = data.docs[0].data().email;
             name = data.docs[0].data().name;
             return next();
         })
@@ -34,4 +33,39 @@ exports.auth = (req, res, next) => {
             }
         })
 
+}
+
+
+exports.basicAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+
+        admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('email', '==', req.user.email)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            email = data.docs[0].data().email;
+            name = data.docs[0].data().name;
+            return next();
+        })
+        .catch(err => {
+            if (err.code === 'auth/argument-error') {
+                return res.status(401).json({ token: 'This is an unauthorized token, please use a valid token' })
+            } else {
+                console.error('Error while verifying token ', err);
+                return res.status(403).json(err);
+            }
+        })
+    } else {
+        name = null;
+        email = null;
+        console.error('No token found')
+        return next();
+        }
 }
