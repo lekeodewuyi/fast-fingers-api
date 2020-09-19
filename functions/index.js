609621922b083e5dc29a6faae221255acf9855d9
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-19 03:53:37 -0400

    adds cors restrictions

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,29 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 const cors = require ('cors');
-app.use(cors());
+// app.use(cors());
 
 const { auth, basicAuth } = require('./utilities/auth')
 const { signup, login, getUser } = require('./handlers/users');
 const { generateText, updatePreference, updateStats, retrieveLeaderBoard } = require('./handlers/data');
 const { postChat, getChats } = require('./handlers/chat');
+
+const corsOption = {
+    origin: 'https://twtr.lekeodewuyi.com',
+    optionsSuccessStatus: 200
+}
+const referrer_domain = "https://twtr.lekeodewuyi.com"
+
+
+app.all('/*', function(req, res, next) {
+    if(req.headers.referer.indexOf(referrer_domain) == -1){
+        console.log("no")
+      res.send('Invalid Request')
+    }
+    next();
+  });
+app.use(cors(corsOption));
+
 
 
 app.post('/', (req, res) => {
